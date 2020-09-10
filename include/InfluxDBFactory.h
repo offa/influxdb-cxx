e89446fbf0217ccc2877424a2c02742df3a897f1
offa <bm-dev@yandex.com>
2020-09-10 16:34:49 +0200

    License header added.

--- include/InfluxDBFactory.h
+++ include/InfluxDBFactory.h
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2019 Adam Wegrzynek
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 ///
 /// \author Adam Wegrzynek
 ///
@@ -17,8 +39,8 @@ class InfluxDBFactory
  public:
    /// Disables copy constructor
    InfluxDBFactory & operator=(const InfluxDBFactory&) = delete;
- 
-   /// Disables copy constructor 
+
+   /// Disables copy constructor
    InfluxDBFactory(const InfluxDBFactory&) = delete;
 
    /// InfluxDB factory
@@ -26,7 +48,7 @@ class InfluxDBFactory
    /// \param url 	URL defining transport details
    /// \throw InfluxDBException 	if unrecognised backend or missing protocol
    static std::unique_ptr<InfluxDB> Get(const std::string& url) noexcept(false);
-  
+
  private:
    ///\return  backend based on provided URL
    static std::unique_ptr<Transport> GetTransport(const std::string& url);
