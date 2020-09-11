fe902ffd7525e26d9841587ec41039e6bb106237
offa <bm-dev@yandex.com>
2020-09-11 20:04:19 +0200

    Loop replaced with algorithm.

--- test/testHttp.cxx
+++ test/testHttp.cxx
@@ -33,11 +33,9 @@ namespace influxdb::test {
 
 int getRandomNumber()
 {
-  std::vector<int> v;
-  for (int i=0; i<1000; i++)
-  {
-    v.push_back(i);
-  }
+  std::vector<int> v(1000);
+  std::iota(v.begin(), v.end(), 0);
+
   std::random_device rd;
   std::mt19937 g(rd());
 
