5c1edccc08f3565069607114c72f605ab684bf00
offa <bm-dev@yandex.com>
2020-09-21 18:40:23 +0200

    Script fixed.

--- script/travis_ci_conan_build.sh
+++ script/travis_ci_conan_build.sh
@@ -9,4 +9,4 @@ fi
 pip install -U --user conan
 conan profile new default --detect
 conan profile update settings.compiler.libcxx=libstdc++11 default
-conan create -o influxdb-cxx:tests={CONAN_BOOST_OPTION} .
+conan create -o influxdb-cxx:tests=${CONAN_BOOST_OPTION} .
