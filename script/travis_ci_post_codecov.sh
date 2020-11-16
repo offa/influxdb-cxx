cc09b03996ca17afee958017cacb16e5d0d7c795
offa <bm-dev@yandex.com>
2020-11-16 13:21:36 +0100

    Travis CI scripts removed (#65).

--- script/travis_ci_post_codecov.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-set -ex
-
-cd ${TRAVIS_BUILD_DIR}/build
-lcov --directory . --capture --output-file coverage.info
-lcov --remove coverage.info '/opt/*' '/usr/*' --output-file coverage.info
-lcov --list coverage.info
-bash <(curl -s https://codecov.io/bash) || echo "No coverage reports"
