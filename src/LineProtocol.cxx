2735da5249769ce13b679fc14936f65c55b4151d
offa <bm-dev@yandex.com>
2021-02-01 17:35:50 +0100

    Line protocol formatting extracted.

---
+++ src/LineProtocol.cxx
@@ -0,0 +1,74 @@
+// MIT License
+//
+// Copyright (c) 2020-2021 offa
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "LineProtocol.h"
+
+namespace influxdb
+{
+    LineProtocol::LineProtocol()
+        : LineProtocol(std::string{})
+    {
+    }
+
+    LineProtocol::LineProtocol(const std::string& tags)
+        : globalTags(tags)
+    {
+    }
+
+    std::string LineProtocol::format(const Point& point)
+    {
+        std::string line{point.getName()};
+
+        if (!globalTags.empty())
+        {
+            line.append(",");
+            line.append(globalTags);
+        }
+
+        if (!point.getTags().empty())
+        {
+            line.append(",");
+            line.append(point.getTags());
+        }
+
+        if (!point.getFields().empty())
+        {
+            line.append(" ").append(point.getFields());
+        }
+
+        return line.append(" ")
+            .append(std::to_string(std::chrono::duration_cast<std::chrono::nanoseconds>(point.getTimestamp().time_since_epoch()).count()));
+    }
+
+    void LineProtocol::addTag(std::string_view key, std::string_view value)
+    {
+        if (!key.empty() && !value.empty())
+        {
+            if (!globalTags.empty())
+            {
+                globalTags.append(",");
+            }
+            globalTags.append(key).append("=").append(value);
+        }
+    }
+
+}
---
+++ test/LineProtocolTest.cxx
@@ -0,0 +1,160 @@
+// MIT License
+//
+// Copyright (c) 2020-2021 offa
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "LineProtocol.h"
+#include <catch2/catch.hpp>
+
+namespace influxdb::test
+{
+    using namespace Catch::Matchers;
+
+    namespace
+    {
+        constexpr std::chrono::time_point<std::chrono::system_clock> ignoreTimestamp(std::chrono::milliseconds(54));
+    }
+
+
+    TEST_CASE("Empty measurement", "[LineProtocolTest]")
+    {
+        const auto point = Point{"p0"}.setTimestamp(ignoreTimestamp);
+        LineProtocol lineProtocol;
+        CHECK_THAT(lineProtocol.format(point), Equals("p0 54000000"));
+    }
+
+    TEST_CASE("Measurement with value", "[LineProtocolTest]")
+    {
+        const auto point = Point{"p0"}.addField("f0", "1").setTimestamp(ignoreTimestamp);
+        LineProtocol lineProtocol;
+        CHECK_THAT(lineProtocol.format(point), Equals(R"(p0 f0="1" 54000000)"));
+    }
+
+    TEST_CASE("Measurement with different types", "[LineProtocolTest]")
+    {
+        const auto point = Point{"multitype"}
+                               .addField("int_value", 567)
+                               .addField("longlong_value", 1234567890LL)
+                               .addField("double_value", 123.4567)
+                               .addField("string_value", "abc def ghi")
+                               .setTimestamp(ignoreTimestamp);
+
+        LineProtocol lineProtocol;
+        CHECK_THAT(lineProtocol.format(point), Matches("multitype "
+                                                       "int_value=567i,"
+                                                       "longlong_value=1234567890i,"
+                                                       "double_value=123.45[0-9]*,"
+                                                       "string_value=\"abc def ghi\""
+                                                       " 54000000"));
+    }
+
+    TEST_CASE("Measurement with multiple values", "[LineProtocolTest]")
+    {
+        const auto point = Point{"multiFieldPoint"}
+                               .addField("value0", 4455)
+                               .addField("value1", 99807)
+                               .addField("value2", 2334)
+                               .setTimestamp(ignoreTimestamp);
+        LineProtocol lineProtocol;
+        CHECK_THAT(lineProtocol.format(point), Equals("multiFieldPoint value0=4455i,"
+                                                      "value1=99807i,"
+                                                      "value2=2334i"
+                                                      " 54000000"));
+    }
+
+    TEST_CASE("Measurement with tag", "[LineProtocolTest]")
+    {
+        const auto point = Point{"taggedPoint"}
+                               .addField("x", 5)
+                               .addTag("tag0", "value0")
+                               .setTimestamp(ignoreTimestamp);
+        LineProtocol lineProtocol;
+        CHECK_THAT(lineProtocol.format(point), Equals(R"(taggedPoint,tag0=value0 x=5i 54000000)"));
+    }
+
+    TEST_CASE("Measurement with multiple tags", "[LineProtocolTest]")
+    {
+        const auto point = Point{"taggedPoint"}
+                               .addField("y", 9)
+                               .addTag("t0", "v0")
+                               .addTag("t1", "v1")
+                               .addTag("t2", "v2")
+                               .setTimestamp(ignoreTimestamp);
+        LineProtocol lineProtocol;
+        CHECK_THAT(lineProtocol.format(point), Equals(R"(taggedPoint,t0=v0,t1=v1,t2=v2 y=9i 54000000)"));
+    }
+
+    TEST_CASE("Adds global tag", "[LineProtocolTest]")
+    {
+        const auto point = Point{"p0"}
+                               .addField("n", 0)
+                               .setTimestamp(ignoreTimestamp);
+        LineProtocol lineProtocol{"global=true"};
+        CHECK_THAT(lineProtocol.format(point), Equals(R"(p0,global=true n=0i 54000000)"));
+    }
+
+    TEST_CASE("Adds global tag to existing tags", "[LineProtocolTest]")
+    {
+        const auto point = Point{"p0"}
+                               .addField("n", 0)
+                               .addTag("local", "1")
+                               .setTimestamp(ignoreTimestamp);
+        LineProtocol lineProtocol{"global=true"};
+        CHECK_THAT(lineProtocol.format(point), Equals(R"(p0,global=true,local=1 n=0i 54000000)"));
+    }
+
+    TEST_CASE("Adds multiple global tags", "[LineProtocolTest]")
+    {
+        const auto point = Point{"p1"}
+                               .addField("n", 1)
+                               .addTag("pointtag", "3")
+                               .setTimestamp(ignoreTimestamp);
+        LineProtocol lineProtocol{"a=0,b=1,c=2"};
+        CHECK_THAT(lineProtocol.format(point), Equals(R"(p1,a=0,b=1,c=2,pointtag=3 n=1i 54000000)"));
+    }
+
+    TEST_CASE("Adds addtional global tag", "[LineProtocolTest]")
+    {
+        const auto point = Point{"p"}
+                               .addField("x", 5)
+                               .addTag("t", "17")
+                               .setTimestamp(ignoreTimestamp);
+        LineProtocol lineProtocol{"global0=aa"};
+        CHECK_THAT(lineProtocol.format(point), Equals(R"(p,global0=aa,t=17 x=5i 54000000)"));
+        lineProtocol.addTag("global1", "bbb");
+        CHECK_THAT(lineProtocol.format(point), Equals(R"(p,global0=aa,global1=bbb,t=17 x=5i 54000000)"));
+    }
+
+    TEST_CASE("Add global tag ignores empty values", "[LineProtocolTest]")
+    {
+        const auto point = Point{"p0"}
+                               .addField("n", 0)
+                               .setTimestamp(ignoreTimestamp);
+        LineProtocol lineProtocol;
+        CHECK_THAT(lineProtocol.format(point), Equals(R"(p0 n=0i 54000000)"));
+        lineProtocol.addTag("", "value0");
+        CHECK_THAT(lineProtocol.format(point), Equals(R"(p0 n=0i 54000000)"));
+        lineProtocol.addTag("key0", "");
+        CHECK_THAT(lineProtocol.format(point), Equals(R"(p0 n=0i 54000000)"));
+        lineProtocol.addTag("", "");
+        CHECK_THAT(lineProtocol.format(point), Equals(R"(p0 n=0i 54000000)"));
+    }
+
+}
