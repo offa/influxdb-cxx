5971402d60e0da2b78628d372b6b7cdb586c40aa
offa <bm-dev@yandex.com>
2022-03-17 20:50:54 +0100

    Update file header

--- test/mock/TransportMock.h
+++ test/mock/TransportMock.h
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (C) 2020-2022  offa
+// Copyright (c) 2020-2022 offa
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
