d85c7913436ca0b4ed67c3c7c76ee65d2b208866
offa <bm-dev@yandex.com>
2023-03-11 17:05:20 +0100

    Update Catch 3.3.2

--- conanfile.py
+++ conanfile.py
@@ -58,7 +58,7 @@ class InfluxdbCxxConan(ConanFile):
         if not self.options.system and self.options.boost:
             self.requires("boost/1.81.0")
         if self.options.tests:
-            self.requires("catch2/3.3.1")
+            self.requires("catch2/3.3.2")
             self.requires("trompeloeil/43")
 
     def generate(self):
