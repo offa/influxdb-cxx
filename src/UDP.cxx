038a085a374d263f18303a37e60ecf69b8ed580d
offa <bm-dev@yandex.com>
2023-04-05 22:10:39 +0200

    UDP packet size

--- src/UDP.cxx
+++ src/UDP.cxx
@@ -28,12 +28,71 @@
 #include "UDP.h"
 #include "InfluxDBException.h"
 #include <string>
+#include <algorithm>
+#include <iterator>
 
 namespace influxdb::transports
 {
+    namespace
+    {
+        std::size_t getSocketSendBufferSize(const boost::asio::ip::udp::socket& socket)
+        {
+            boost::asio::ip::udp::socket::send_buffer_size sendBufferSizeOption;
+            socket.get_option(sendBufferSizeOption);
+            int sendBufferSize{sendBufferSizeOption.value()};
+            return (sendBufferSize >= 0 ? static_cast<std::size_t>(sendBufferSize) : 0U);
+        }
+
+        std::size_t getMaxMessageSize(const boost::asio::ip::udp::socket& s)
+        {
+            constexpr std::size_t maxLengthValue{(std::numeric_limits<std::uint16_t>::max)()};
+            constexpr std::size_t udpHeaderSize{8};
+            constexpr std::size_t ipv4HeaderSize{20};
+            constexpr std::size_t maxUDPDataSize{maxLengthValue - udpHeaderSize - ipv4HeaderSize};
+
+            return std::min(maxUDPDataSize, getSocketSendBufferSize(s));
+        }
+
+
+        template <class T>
+        auto first(T itr1, T itr2)
+        {
+            return std::distance(itr1, itr2) > 0 ? itr1 : itr2;
+        }
+
+        void sendChunks(boost::asio::ip::udp::socket& socket, boost::asio::ip::udp::endpoint& endpoint, std::string message, std::size_t limit)
+        {
+            auto begin = message.begin();
+            while (true)
+            {
+                const auto end = first(std::next(begin, limit), message.end());
+
+                if (end == message.end())
+                {
+                    socket.send_to(boost::asio::buffer(std::string{begin, end}), endpoint);
+                    return;
+                }
+
+                const auto eol = std::find(std::make_reverse_iterator(end), std::make_reverse_iterator(begin), '\n');
+
+                if (eol.base() != begin)
+                {
+                    socket.send_to(boost::asio::buffer(std::string{begin, eol.base()}), endpoint);
+                    begin = std::next(eol.base());
+                }
+                else
+                {
+                    begin = std::next(std::find(end, message.end(), '\n'));
+                }
+            }
+        }
+
+    }
+
 
     UDP::UDP(const std::string& hostname, int port)
-        : mSocket(mIoService, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), 0))
+        : mSocket(mIoService, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), 0)),
+          maxMessageSize(getMaxMessageSize(mSocket))
     {
         boost::asio::ip::udp::resolver resolver(mIoService);
         boost::asio::ip::udp::resolver::query query(boost::asio::ip::udp::v4(), hostname, std::to_string(port));
@@ -45,7 +104,14 @@ namespace influxdb::transports
     {
         try
         {
-            mSocket.send_to(boost::asio::buffer(message, message.size()), mEndpoint);
+            if (message.size() < maxMessageSize)
+            {
+                mSocket.send_to(boost::asio::buffer(message, message.size()), mEndpoint);
+            }
+            else
+            {
+                sendChunks(mSocket, mEndpoint, std::move(message), maxMessageSize);
+            }
         }
         catch (const boost::system::system_error& e)
         {
