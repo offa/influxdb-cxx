b9b09e62e02a9f71befa103cb9ae3aa3d4ac938d
offa <bm-dev@yandex.com>
2024-06-12 20:21:47 +0200

    Update Boost 1.85.0

--- conanfile.py
+++ conanfile.py
@@ -19,7 +19,7 @@ class InfluxdbCxxConan(ConanFile):
     def requirements(self):
         self.requires("cpr/1.10.5")
         if not self.options.system and self.options.boost:
-            self.requires("boost/1.83.0")
+            self.requires("boost/1.85.0")
         if self.options.tests:
             self.requires("catch2/3.6.0")
             self.requires("trompeloeil/47")
