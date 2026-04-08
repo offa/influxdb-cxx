441136e8d37a3a06b64782529fc25523a9c77af5
offa <8887756+offa@users.noreply.github.com>
2026-04-08 18:05:00 +0000

    Update Catch 3.14.0 (#352)
    Remove Clang 22 workaround

--- cmake/clang22_workaround.cmake
+++
@@ -1,6 +0,0 @@
-function(clang22_workaround_target targetname)
-    # Workaround for catch2 #3076
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "22")
-        target_compile_options(${targetname} PRIVATE -Wno-c2y-extensions)
-    endif()
-endfunction()
