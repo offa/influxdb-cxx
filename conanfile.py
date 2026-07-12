b8f36e69d468562aa0dc8f31a1e4a2abaacdb8f4
offa <8887756+offa@users.noreply.github.com>
2026-07-12 19:52:29 +0200

    Update Catch 3.15.2

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,11 @@
 from conan import ConanFile
 
+
 class InfluxdbCxxConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
 
-    options = {
-        "tests": [True, False],
-        "system": [True, False],
-        "boost": [True, False]
-    }
+    options = {"tests": [True, False], "system": [True, False], "boost": [True, False]}
     default_options = {
         "tests": True,
         "system": False,
@@ -21,5 +18,5 @@ class InfluxdbCxxConan(ConanFile):
         if not self.options.system and self.options.boost:
             self.requires("boost/1.88.0")
         if self.options.tests:
-            self.requires("catch2/3.15.1")
+            self.requires("catch2/3.15.2")
             self.requires("trompeloeil/49")
